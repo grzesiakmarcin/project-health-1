dba0e4404e0985f2cf62208720346b772a6141ac
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-11-21 18:02:17 +0100

    First commit

---
+++ src/main/java/com/github/java/demo/ProjectHealthApplication.java
@@ -0,0 +1,13 @@
+package com.github.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/java/demo/ProjectHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.java.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
